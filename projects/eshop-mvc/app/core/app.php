e80849e6f55e7cb4c5d06093076ade5314f9593e
samhayder <sams.seul@gmail.com>
2024-11-16 23:41:28 +0600

    MVC-OOP boilerplate

--- projects/eshop-mvc/app/core/app.php
+++ projects/eshop-mvc/app/core/app.php
@@ -4,11 +4,30 @@ class App
 {
   protected $controller = "home";
   protected $method = "index";
+  protected $params = [];
 
   public function __construct()
   {
     $url = $this->parseUrl();
-    showData($url);
+
+    if (file_exists("../app/controllers/" . strtolower($url[0]) . ".php")) {
+      $this->controller = strtolower($url[0]);
+      unset($url[0]);
+    }
+
+    require "../app/controllers/" . $this->controller . ".php";
+    $this->controller = new $this->controller;
+
+    if (isset($url[1])) {
+      if (method_exists($this->controller, strtolower($url[1]))) {
+        $this->method = strtolower($url[1]);
+        unset($url[1]);
+      }
+    }
+
+    $this->params = (count($url) > 0) ? $url : ["home"];
+    call_user_func_array([$this->controller, $this->method], $this->params);
+
   }
 
   private function parseUrl()
--- projects/eshop-mvc/app/core/controller.php
+++ projects/eshop-mvc/app/core/controller.php
@@ -1,1 +1,9 @@
 <?php
+
+class Controller {
+  public function view($path, $data = []) {
+    if (file_exists("../app/views/" . $path . ".php")){
+      include "../app/views/" . $path . ".php";
+    }
+  }
+}
